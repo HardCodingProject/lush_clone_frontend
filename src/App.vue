3f67289905e823634bf3d9387fdd75305ad46574
[Insa14] <[kong3361@naver.com]>
2021-09-09 00:34:54 +0900

    app.vue 기본base

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <router-link to="/">메인</router-link>
+    <router-link to="/join">회원가입</router-link>
+    <hr />
+    <router-view></router-view>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
--- src/components/Join.vue
+++ src/components/Join.vue
@@ -1,6 +1,7 @@
 <template>
     <div>
-
+        join
+    
     </div>
 </template>
 
