fcc89817834e5687989fd1648786f43a93d05a62
[Insa14] <[kong3361@naver.com]>
2021-09-09 00:32:27 +0900

    router설정

---
+++ src/routes/index.js
@@ -0,0 +1,16 @@
+import { createWebHistory, createRouter } from "vue-router";
+
+import Home from '@/components/Home.vue';
+import Join from '@/components/Join.vue';
+
+const routes = [
+    {path : '/' , name :'Home', component:Home},
+    {path : '/join' , name :'Join', component:Join},
+]
+
+const router = createRouter({
+    history : createWebHistory(),
+    routes,
+})
+
+export default router;
