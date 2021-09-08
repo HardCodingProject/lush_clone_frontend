58163d688b98baa33d0b62672b9ff5ab552c7ab4
kaydan95 <kyori0515@gmail.com>
2021-09-09 00:54:55 +0900

    0909_수정
    기본 모듈 설치, index.js vue.config.js 부분등

--- src/main.js
+++ src/main.js
@@ -4,8 +4,12 @@ import App from './App.vue'
 // router
 import router from './routes';
 
+// socket
+// import io from 'socket.io-client';
+// const socket = io("/", { transports : ['websocket']});
+
 const app = createApp(App);
 
 app.use(router);  //router
-
+// app.config.globalProperties.$socket = socket;
 app.mount('#app');
