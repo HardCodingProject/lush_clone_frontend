3e5c9e4ac67a6e1ddc28fdca0fb676688fb8ad75
[Insa14] <[kong3361@naver.com]>
2021-09-11 01:44:29 +0900

    dropdown나타내기, grid영역 잡기
    현재 grid로 영역은 나눴으며, dropdown또한 div를 이벤트를 통해 나타나게 했다. 이벤트 발생 시 header에 height가 높아지는데 해당 현상 해결중

--- src/App.vue
+++ src/App.vue
@@ -1,156 +1,25 @@
 <template>
-  <div>
-    <header>
-      
-        <span id="logo">LUSH</span>
-     
-      <ul id="nav"> 
-        <li><a href="#">제품</a>
-          <ul>
-            <li><a href="#">best</a></li>
-            <li><a href="#">best</a></li>
-          </ul>
-        </li> 
-          <li><a href="#">제품</a>
-          <ul>
-            <li><a href="#">best</a></li>
-            <li><a href="#">best</a></li>
-          </ul>
-        </li> 
-          <li><a href="#">제품</a>
-          <ul>
-            <li><a href="#">best</a></li>
-            <li><a href="#">best</a></li>
-          </ul>
-        </li> 
-      </ul> 
-        <div style="display:inline-block">
-          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSWLvNMChvJvdDEZrJhoBuILPzrg4FX2u3Y_g&usqp=CAU"/>
-        </div>
-        <div style="display:inline-block">
-          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQWeWGfZ947mDkWMwt5zHCQIIVqr1rgunc8Hw&usqp=CAU" />
-        </div>
-    
-    </header>
-        
-    <main></main>
-    <fotter></fotter>
-
-
-    
-  </div>
-</template>
-
-<script>
-export default {
-  data(){
-    return {
-    }
-  }
-}
-</script>
-
-<style>
-header{
-  color: white;
-  background-color: black;
-  margin: 0 auto;
-  vertical-align: middle;
-
-  width: 100%;
-}
-
-#logo{
-  font-family: Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
-  font-size: 30px;
-  letter-spacing: 5px;
-  display: inline-block;
-  height: 80px;
-  margin-left: 15%;
-}
-img{
-  width: 30px;
-  height: 30px;
-  display: inline-block;
-}
-#nav{
-  border: solid #fff 1px;
-  display: inline-block;
-  list-style: none;
-  margin: 0;
-  margin-top: 30px;
-  margin-left: 15%;
-}
-#nav li{
-  float: left;
-  position: relative;
-  margin: 5px 30px;
-  padding: 0;
-}
-#nav li a{
-  display: block;
-  font-family: "맑은고딕";
-  font-size: 15px;
-  padding: 7px 8px;
-  margin: 0;
-  color: #fff;
-  text-decoration: none;
-}
-#nav li:hover > a{
-  transition: color .2s;
-  color: rgb(255, 255, 255);
-  margin: 0;
-  padding: 5px;
-}
-#nav ul{
-  list-style: none;
-  margin: 0;
-  margin-top: 17px;
-  padding: 0;
-  position: absolute;
-  left: 0;
-  top: 40px;
-  width: 800px;
-  background: rgb(71, 71, 71);
-  text-align: center;
-  opacity: 0;
-}
-#nav ul li{
-  float: none;
-  margin: 0;
-  padding: 0;
-  font-size: 10px;
-}
-#nav ul a{
-  font-weight: normal;
-  color: rgb(194, 194, 194);
-}
-#nav ul li:hover a{
-  font-size: 1.1rem;
-}
-#nav li:hover ul{
-  opacity: 1;
-}
-#nav li:hover ul li{
-  height: 35px;
-  overflow: visible;
-  padding: 0;
-}
-</style>
---------------------------------------------------------------------
-<template>
   <div class="wrapper">
-  <div class="box header">
-    
-  </div>
-  <div class="box sidebar">Sidebar</div>
-  <div class="box content">Content
-    <br /> The four arrows are inline images inside the content area.
-    <img src="http://gridbyexample.com/examples/code/arrow-top-left.png" alt="top left" class="topleft" />
-    <img src="http://gridbyexample.com/examples/code/arrow-top-right.png" alt="top right" class="topright" />
-    <img src="http://gridbyexample.com/examples/code/arrow-bottom-left.png" alt="bottom left" class="bottomleft" />
-    <img src="http://gridbyexample.com/examples/code/arrow-bottom-right.png" alt="bottom right" class="bottomright" /></div>
-  <div class="box footer">Footer</div>
+    <div class="header">
+      <span id="logo">LUSH</span>
+      <div id="menu">
+        <a href="#" @mouseover="viewNav(0)" @mouseout="hiddenNave(0)">제품</a>
+        <a href="#" >러쉬 소개</a> 
+        <a href="#">매장 안내</a>
+      </div>
+
+      <span id="logo">LUSH</span>
+    </div>
+
+    <div class="content">
+      <div v-if="product" id="product">
+        
+      </div>
+      <div v-if="introduce" id="introduce">
+        
+      </div>
+    </div>
+    <div class="footer">Footer</div>
 </div>
 </template>
 
@@ -158,26 +27,53 @@ img{
 export default {
   data(){
     return {
+      product   : false, // 제품 서브나브
+      introduce : true, // 소개
+    }
+  },
+  methods: {
+    viewNav(val){
+      if(val === 0){
+        this.product = true;
+      }
+      if(val === 1){
+        this.introduce = true;
+      }
+    },
+    hiddenNave(val){
+      if(val === 0){
+        this.product = false;
+      }
+      if(val === 1){
+        this.introduce = false;
+      }
     }
   }
 }
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@900&display=swap');
 body {
-  margin: 40px;
+  margin: 0;
 }
-
-.sidebar {
-  grid-area: sidebar;
+#logo{
+  font-family: 'Roboto', sans-serif;
+  font-size: 30px;
+  color: white;
+  letter-spacing: 2px;
+  padding-left: 90px;
 }
 .content {
   grid-area: content;
-  position: relative;
+  background-color: rgb(205, 228, 79);
 }
 
 .header {
   grid-area: header;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  place-items: center;;
 }
 
 .footer {
@@ -185,52 +81,32 @@ body {
 }
 
 .wrapper {
-    display: grid;
-    grid-gap: 10px;
-    grid-template-columns: 50% 100% 300px;
-    grid-template-rows: 80px 500px 460px;
-    grid-template-areas:
-    "header  header  header"
-    "sidebar content content"
-    "footer  footer  footer";
-    background-color: rgb(167, 89, 89);
-    color: #444;
-  }
-
-
-.box {
-  background-color: rgb(205, 228, 79);
-  color: #fff;
-  border-radius: 5px;
-  font-size: 150%;
+  display: grid;
+  grid-template-columns: 100%;
+  grid-template-rows: 50% 300% 100px; 
+  grid-template-areas:
+  "header"
+  "content"
+  "footer";
+  background-color: rgb(167, 89, 89);
+  color: #444;
 }
 
 .header,
 .footer {
   background-color: rgb(0, 0, 0);
 }
-
-.topleft {
-  position: absolute;
-  top: 0;
-  left: 0;
+#product, #introduce{
+  width: 100%;
+  height: 100px;
+  background: rgb(71, 71, 71);
+  position: relative;
 }
-
-.topright {
-  position: absolute;
-  top: 0;
-  right: 0;
-}
-
-.bottomleft {
-  position: absolute;
-  bottom: 0;
-  left: 0;
-}
-
-.bottomright {
-  position: absolute;
-  bottom: 0;
-  right: 0;
+#menu a{
+  text-decoration: none;
+  color: #fff;
+  font-family: 'Roboto', sans-serif;
+  width: 20px;
+  padding: 20px;
 }
 </style>
