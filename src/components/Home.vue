499dc3c85f43d281b055e2f641cce48a60c6d93c
kaydan95 <kyori0515@gmail.com>
2021-09-19 04:52:59 +0900

    0919_수정

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -11,9 +11,24 @@
             <div class="inner_wrap">
                 <div class="display_list">
                     <h2>나만 알고 싶은 향기</h2>
+                    <div class="products_list">
+                        <el-carousel trigger="click" height="300px">
+                            <el-carousel-item v-for="item in 4" :key="item">
+                                <el-row :gutter="15">
+                                    <el-col :span="5">
+                                        <div class="grid-content bg-purple">
+                                            <img :src="best_product_1">
+                                        </div>
+                                    </el-col>
+                                    <el-col :span="5"><div class="grid-content bg-purple"></div></el-col>
+                                    <el-col :span="5"><div class="grid-content bg-purple"></div></el-col>
+                                </el-row>
+                            </el-carousel-item>
+                        </el-carousel>
+                    </div>
                 </div>
                 <div class="review_float">
-
+                    
                 </div>
             </div>
         </div>
@@ -30,6 +45,7 @@ import main_banner_3 from '@/assets/main_banner_3.jpg';
 import main_banner_4 from '@/assets/main_banner_4.jpg';
 import main_banner_5 from '@/assets/main_banner_5.jpg';
 import main_banner_6 from '@/assets/main_banner_6.jpg';
+import best_product_1 from '@/assets/best_product_1.jpg';
 
     export default {
         data(){
@@ -40,6 +56,8 @@ import main_banner_6 from '@/assets/main_banner_6.jpg';
                     main_banner_4 : main_banner_4,
                     main_banner_5 : main_banner_5,
                     main_banner_6 : main_banner_6,
+                    best_product_1 : best_product_1,
+
                 banners : [
                     main_banner_1,
                     main_banner_2,
@@ -57,6 +75,7 @@ import main_banner_6 from '@/assets/main_banner_6.jpg';
 </script>
 
 <style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@900&display=swap');
 .container{
     margin: 0;
     padding: 0;
@@ -72,7 +91,7 @@ import main_banner_6 from '@/assets/main_banner_6.jpg';
 .main_banner_1, .main_banner_2, .main_banner_3, .main_banner_4 {
     width: 100vw;
     /* height: 100%; */
-    border: 1px solid black;
+    /* border: 1px solid black; */
 
 }
 .main_banner_2{
@@ -83,16 +102,50 @@ import main_banner_6 from '@/assets/main_banner_6.jpg';
 .main_banner_2 .inner_wrap{
     width: 100%;
 }
+.main_banner_2 .inner_wrap .display_list {
+    margin-left: 100px;
+}
+.main_banner_2 .inner_wrap .display_list > h2 {
+    font-size: 35px;
+    color: black;
+    font-weight: bold;
+    font-family: 'Roboto', sans-serif;
+
+}
+.main_banner_2 .inner_wrap .display_list .products_list el-carousel-item{
+
+}
 .el-carousel__item img {
     height: 100%;
     width: 100vw;
 }
-
-.el-carousel__item:nth-child(2n) {
-background-color: #99a9bf;
+.el-row {
+    /* margin-bottom: 20px; */
+    margin-left: 20px;
 }
+.el-col {
+border-radius: 4px;
+}
+.bg-purple-dark {
+    height: 100%;
+    background: #99a9bf;
+}
+.bg-purple {
+    background: #d3dce6;
+}
+.bg-purple img {
+    height: 100%;
 
-.el-carousel__item:nth-child(2n + 1) {
-background-color: #d3dce6;
+}
+.bg-purple-light {
+    background: #e5e9f2;
+}
+.grid-content {
+border-radius: 4px;
+min-height: 36px;
+}
+.row-bg {
+padding: 10px 0;
+background-color: #f9fafc;
 }
 </style>
