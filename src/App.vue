d018f38ffcd32c5dbc7d867574706c014cf79b6d
[Insa14] <[kong3361@naver.com]>
2021-09-20 20:16:16 +0900

    menu수정중

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,34 @@
 <template>
   <div class="wrapper">
     <div class="header">
-      <div id="logo">LUSH</div>
-      <nav>
-        <ul>
-          <li><a href="#">제품</a></li>
-          <li><a href="#">러쉬소개</a>
-            <ul>
-              <li><a href="#">러쉬소개</a>
-               <ul>
-                  <li><a href="#">러쉬역사</a></li>
-                  <li><a href="#">이념과가치</a></li>
-               </ul>
-              </li>
-              <li><a href="#">채러티팟</a></li>
-              <li><a href="#">커뮤니티</a>
-                <ul>
-                  <li><a href="#">젤러쉬</a></li>
-                  <li><a href="#">덕찌</a></li>
-                </ul>
-              </li>
-              <li><a href="#">서명캠페인</a></li>
-              <li><a href="#">기업선물</a></li>
-            </ul>
-          </li>
-        </ul>
-      </nav>
-      <div class="img">
-        <img :src = 'search' id="img1">
-        <img :src = 'shop' id="img2">
-        <img :src = 'login' id="img3">
+      <div class="navbar">
+        <a href="#home">LUSH</a>
+        <a href="#news">제품</a>
+        <div class="dropdown">
+          <button class="dropbtn">러쉬소개
+            <i class="fa fa-caret-down"></i>
+          </button>
+          <div class="dropdown-content">
+            <div class="row">
+              <div class="column">
+                <h3>러쉬소개</h3>
+                <a href="#">러쉬 역사</a>
+                <a href="#">이념과 가치</a>
+              </div>
+              <div class="column">
+                <h3>채러티팟</h3>
+                <a href="#">Link 1</a>
+                <a href="#">Link 2</a>
+                <a href="#">Link 3</a>
+              </div>
+              <div class="column">
+                <h3>커뮤니티</h3>
+                <a href="#">젤러쉬</a>
+                <a href="#">덕찌</a>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
 
@@ -64,41 +63,6 @@ body {
   margin: 0;
   width: 100%;
 }
-#logo{
-  font-family: 'Roboto', sans-serif;
-  font-size: 30px;
-  color: white;
-  letter-spacing: 2px;
-  padding-left: 160px;
-}
-.img{
-  /* padding-right: 200px; */
-  width:max-content;
-}
-#img1{
-  width: 25px;
-  height: 25px;
-  margin-right: 40px;
-}
-#img2{
-  width:30px;
-  height: 30px;
-  margin-right: 40px;
-}
-#img3{
-  width: 30px;
-  height: 30px;
-  margin-right: 30px;
-}
-#logo{
-  font-family: 'Roboto', sans-serif;
-  font-size: 30px;
-  color: white;
-  letter-spacing: 2px;
-  display: inline-block;
-  /* padding-left: 160px; */
-}
-
 .content {
   width: 100vw;
   grid-area: content;
@@ -108,7 +72,8 @@ body {
 
 .header {
   grid-area: header;
-  display: inline-block;
+  display: grid;
+  place-items: center;
   background-color: rgb(0, 0, 0);
 }
 
@@ -122,76 +87,88 @@ body {
   background-color: rgb(167, 89, 89);
   color: rgb(68, 68, 68);
 }
-nav {
-  text-align:center;
-  width: 500px;
-  background: #000000;
-  padding: 0;
-  margin: 0;
-  height: 60px;
-  position:relative;
-  z-index: 10000;
-  display: inline-block;
-}
-nav ul {
-  background: #000000;
-  list-style:none;
-  padding:0 20px;
-  margin: 0;
-  height: 80px;
-}
-nav ul li {
-  display: inline-block;
-}
-nav ul li a {
-  color:#ffffff;
-  display:block;
-  padding:0px 40px;
-  text-decoration:none;
-  float: left;
-  height: 60px;
-  line-height: 60px;
-}
-nav ul li ul a {
-  color:#807e7e;
-}
-nav ul li ul li a{
-  color: #FFFFFF;
-}
-nav ul li a:hover{
-    color:#FFFFFF;
-}
-nav ul li ul li a:hover{
-    color:#FFFFFF;
-}
-nav ul li ul li ul li a:hover > {
-    color:#FFFFFF;
-    transition: color .2s;
+.navbar {
+  overflow: hidden;
+  font-family: Arial;
 }
 
-nav ul ul {
-  background: #333;
-  padding:0;
+/* Links inside the navbar */
+.navbar a {
+  float: left;
+  font-size: 16px;
+  color: white;
   text-align: center;
-  display: none;
-  width: 100%;
-  position: absolute;
-  top: 60px;
-  left: 0px;
-}
-nav ul li:hover > ul {
-  display:block;
-}
-nav ul li ul li ul{
-  margin: 0;
-  padding: 0;
-  list-style: none;
-}
-nav ul li ul li ul li a{
-  display: block;
-  padding: 5px;
-  color:  rgb(146, 146, 146);
-  font-size: 14px;
+  padding: 30px 16px;
   text-decoration: none;
+}
+
+/* The dropdown container */
+.dropdown {
+  float: left;
+  overflow: hidden;
+}
+
+/* Dropdown button */
+.dropdown .dropbtn {
+  font-size: 16px;
+  border: none;
+  outline: none;
+  color: white;
+  padding: 30px 16px;
+  background-color: inherit;
+  font: inherit; /* Important for vertical align on mobile phones */
+  margin: 0; /* Important for vertical align on mobile phones */
+}
+
+/* Dropdown content (hidden by default) */
+.dropdown-content {
+  display: none;
+  position: absolute;
+  background-color: #333;
+  
+  width: 100%;
+  left: 0;
+  box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+  z-index: 10;
+}
+
+/* Show the dropdown menu on hover */
+.dropdown:hover .dropdown-content {
+  display: block;
+  padding: 16px;
+}
+
+/* Create three equal columns that floats next to each other */
+.column {
+  float: left;
+  margin-left: 50px;
+  width: 20%;
+  height: 250px;
+}
+.column h3 {
+  color: rgb(255, 255, 255);
+}
+
+/* Style links inside the columns */
+.column a {
+  float: none;
+  color: rgb(146, 146, 146);
+  padding: 16px;
+  text-decoration: none;
+  display: block;
+  text-align: left;
+}
+
+/* Add a background color on hover */
+.column a:hover {
+  color: rgb(255, 255, 255);
+  transition: color .2s;
+}
+
+/* Clear floats after the columns */
+.row:after {
+  content: "";
+  display: table;
+  clear: both;
 }
 </style>
