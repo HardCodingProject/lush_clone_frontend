5e2da311e15d287961919ca339d29020169f438c
[Insa14] <[kong3361@naver.com]>
2021-09-20 23:33:38 +0900

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -277,13 +277,14 @@ body {
   display: inline-block;
 }
 .dropdown-content2 {
-  display: none;
+  display: block;
   position: absolute;
   right: 0;
   background-color: #f9f9f9;
   min-width: 160px;
   box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
-  z-index: 100;
+  transform: translateY(10px);
+  z-index: 1000;
 }
 .dropdown-content2 a {
   color: black;
