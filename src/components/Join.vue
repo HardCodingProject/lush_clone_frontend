2da34c0874fe9383d60b6fca962c90b7ce37c33a
kaydan95 <kyori0515@gmail.com>
2021-09-26 01:06:37 +0900

    0926_join 수정
    회원가입 부분 다음 우편번호 api 추가 및 완성

--- src/components/Join.vue
+++ src/components/Join.vue
@@ -163,8 +163,8 @@
     </div>
 </template>
 
-
 <script>
+
 import Footer from '@/components/Footer.vue';
 import select_arrow_down from '@/assets/select_arrow_down.png';
     export default {
@@ -172,8 +172,16 @@ import select_arrow_down from '@/assets/select_arrow_down.png';
             return{
                 isOpen :false,
                 selected : '',
-                select_arrow_down : select_arrow_down
+                select_arrow_down : select_arrow_down,
+                postcode :'',
+                roadAddress :'',
+                detailAddress : '',
             }
+        },
+        mounted() {
+            let daumPostCode = document.createElement('script')
+            daumPostCode.setAttribute('src', '//t1.daumcdn.net/mapjsapi/bundle/postcode/prod/postcode.v2.js')
+            document.head.appendChild(daumPostCode);
         },
         components:{
             Footer : Footer,
@@ -183,50 +191,44 @@ import select_arrow_down from '@/assets/select_arrow_down.png';
                 this.isOpen = !this.isOpen;
             },
             openDaumPostCode(){
-                const elementWrap = this.wrap;
-                // 현재 scroll 위치를 저장해놓는다.
-                const currentScroll = Math.max(document.body.scrollTop, document.documentElement.scrollTop);
                 new window.daum.Postcode({
                     oncomplete: (data) => {
                         // 검색결과 항목을 클릭했을때 실행할 코드를 작성하는 부분.
             
                         // 각 주소의 노출 규칙에 따라 주소를 조합한다.
                         // 내려오는 변수가 값이 없는 경우엔 공백('')값을 가지므로, 이를 참고하여 분기 한다.
-                        let addr = data.roadAddress; // 주소 변수
+                        let addr = data.query; // 주소 변수
                         let postcode = data.zonecode;
+                        let extraRoadAddr = ''
             
-                        // 사용자가 선택한 주소 타입에 따라 해당 주소 값을 가져온다.
-                        if (data.userSelectedType === 'R') { // 사용자가 도로명 주소를 선택했을 경우
-                            addr = data.roadAddress;
-                        } else { // 사용자가 지번 주소를 선택했을 경우(J)
-                            addr = data.jibunAddress;
+                        // 법정동명이 있을 경우 추가한다. (법정리는 제외)
+                        // 법정동의 경우 마지막 문자가 "동/로/가"로 끝난다.
+                        if(data.bname !== '' && /[동|로|가]$/g.test(data.bname)){
+                            extraRoadAddr += data.bname;
+                        }
+                        // 건물명이 있고, 공동주택일 경우 추가한다.
+                        if(data.buildingName !== '' && data.apartment === 'Y'){
+                            extraRoadAddr += ( extraRoadAddr !== '' ? ', ' + data.buildingName : data.buildingName);
+                        }
+                        // 표시할 참고항목이 있을 경우, 괄호까지 추가한 최종 문자열을 만든다.
+                        if( extraRoadAddr !== ''){
+                            extraRoadAddr = ' (' + extraRoadAddr + ')';
                         }
             
                         // 우편번호와 주소 정보를 해당 필드에 넣는다.
                         // document.getElementById('address').value = addr
                         this.postcode = postcode;
-                        this.address = addr;
-            
-                        // iframe을 넣은 element를 안보이게 한다.
-                        // (autoClose:false 기능을 이용한다면, 아래 코드를 제거해야 화면에서 사라지지 않는다.)
-                        elementWrap.style.display = 'none';
-            
-                        // 우편번호 찾기 화면이 보이기 이전으로 scroll 위치를 되돌린다.
-                        document.body.scrollTop = currentScroll;
+                        this.roadAddress = addr;
+                        
+                        // 참고항목 문자열이 있을 경우 해당 필드에 넣는다.
+                        if(addr !== ''){
+                            this.detailAddress = extraRoadAddr;
+                        } else {
+                            this.detailAddress = '';
+                        }
                     },
-                    // 우편번호 찾기 화면 크기가 조정되었을때 실행할 코드를 작성하는 부분. iframe을 넣은 element의 높이값을 조정한다.
-                    onresize: function (size) {
-                        elementWrap.style.height = size.height + 'px';
-                    },
-                    width: '100%',
-                    height: '100%'
-                }).embed(elementWrap);
-                elementWrap.style.display = 'block';
+                }).open()
             },
-            foldDaumPostcode () {
-                const elementWrap = this.$refs.wrap;
-                elementWrap.style.display = 'none';
-            }
 
         }
     }
