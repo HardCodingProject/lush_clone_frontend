63c4b784c0f5aad646dc52faec25d59e45599edf
[Insa14] <[kong3361@naver.com]>
2021-09-26 23:54:33 +0900

    component로 불러오기

---
+++ src/components/MenuShop.vue
@@ -0,0 +1,334 @@
+<template>
+    <div>
+        <div class="menu1">
+            <div class="member-img">
+                <img src="https://lush.co.kr/data/icon/group_image/ico_member_upload1.png" alt="">
+            </div>
+            <div class="member-text">
+                <p>정지희님의</p>
+                <p>회원등급은 일반회원등급 입니다.</p>
+            </div>
+            <div class="cupon">
+                <img :src="icon1" class="icon-ticket">
+                <p>혜택</p>
+            </div>
+            <div class="duczzi">
+                <img :src="icon2" class="icon-smile">
+                <p>덕찌</p>
+            </div>
+            <div class="qna">
+                <img :src="icon3" class="icon-chat">
+                <p>문의</p>
+            </div>
+        </div>
+        <div class="menu2">
+            <div class="menu2-header">
+                <h2>진행 중인 주문</h2>
+                <p>최근 30일 내에 진행중인 주문정보입니다.</p>
+            </div>
+            <div class="menu2-body">
+                <ol class="clear"> 
+                    <li>
+                        <div class="num">
+                            <span>0</span>
+                        </div>
+                        <p class="num-text">입금대기</p>
+                        <span class="icon-arr">&nbsp;</span>
+                    </li>
+                    <li>
+                        <div class="num">
+                            <span>0</span>
+                        </div>
+                        <p class="num-text">결제완료</p>
+                        <span class="icon-arr">&nbsp;</span>
+                    </li>
+                    <li>
+                        <div class="num">
+                            <span>0</span>
+                        </div>
+                        <p class="num-text">상품준비중</p>
+                        <span class="icon-arr">&nbsp;</span>
+                    </li>
+                    <li>
+                        <div class="num">
+                            <span>0</span>
+                        </div>
+                        <p class="num-text">배송중</p>
+                        <span class="icon-arr">&nbsp;</span>
+                    </li>
+                    <li>
+                        <div class="num">
+                            <span>0</span>
+                        </div>
+                        <p class="num-text">배송완료</p>
+                        <span class="icon-arr">&nbsp;</span>
+                    </li>
+                    <li>
+                        <div class="num">
+                            <span>0</span>
+                        </div>
+                        <p class="num-text">구매확정</p>
+                    </li>
+                </ol>
+                <div class="order-total">
+                    <div class="order1">
+                        <span class="title">취소</span>
+                        <span class="num1">
+                            <span>0</span>
+                            건
+                        </span>
+                    </div>
+                    <div class="order1">
+                        <span class="title">교환</span>
+                        <span class="num1">
+                            <span>0</span>
+                            건
+                        </span>
+                    </div>
+                    <div class="order1"> 
+                        <span class="title">반품</span>
+                        <span class="num1">
+                            <span>0</span>
+                            건
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="menu2">
+            <div class="menu2-header">
+                <h2>최근 주문 정보</h2>
+                <p>최근 30일 내에 주문하신 내역입니다.</p>
+            </div>
+            <div class="menu3-body">
+                <table class="goods-board"> 
+                    <thead>
+                        <tr>
+                            <th>날짜/주문번호</th>
+                            <th>상품명/옵션</th>
+                            <th>상품금액/수량</th>
+                            <th>주문상태</th>
+                            <th>확인/리뷰</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td colspan="5" class="no-data">조회내역이 없습니다.</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+        <div class="menu2">
+            <div class="menu2-header">
+                <h2>최근 본 상품</h2>
+                <p>정지희님께서 본 최근 상품입니다.</p>
+            </div>
+            <div class="menu3-body">
+                <table class="goods-board1"> 
+                    <tbody>
+                        <tr>
+                            <td colspan="5" class="no-data">상품이 존재하지 않습니다.</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import icon1 from '@/assets/ticket.png';
+import icon2 from '@/assets/duczzi.png';
+import icon3 from '@/assets/qna.png';
+    export default {
+        data(){
+            return{
+                icon1 : icon1,
+                icon2 : icon2,
+                icon3 : icon3,
+            }
+        },
+    }
+</script>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@900&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Nanum+Gothic:wght@800&display=swap');
+
+.menu1{
+    border: 1px solid #e9e9e9;
+    padding: 20px 20px 20px 10px;
+}
+.member-img{
+    display: table-cell;
+    width: 120px;
+    height: 30px;
+    text-align: center;
+    vertical-align: middle;
+}
+img{
+    height: 70px;
+    width: 70px;
+}
+.member-text{
+    display: table-cell;
+    font-size: 14px;
+    font-family: 'Nanum Gothic', sans-serif;
+    width: 350px;
+}
+.cupon, .duczzi, .qna{
+    display: table-cell;
+    width: 146px;
+    height: 77px;
+}
+.icon-ticket{
+    width: 35px;
+    height: 25px;
+    background-position: 0 0;
+}
+.icon-smile{
+    width: 43px;
+    height: 25px;
+    background-position: 0 0;
+}
+.icon-chat{
+    width: 35px;
+    height: 25px;
+    background-position: 0 0;
+}
+.menu2{
+    margin-top: 40px;
+}
+.menu2-header{
+    margin-bottom: 10px;
+}
+.menu2-header h2{
+    display: inline-block;
+    vertical-align: middle;
+    font-family: 'Nanum Gothic', sans-serif;
+    font-size: 17px;
+    color: #111;
+}
+.menu2-header p{
+    display: inline-block;
+    vertical-align: middle;
+    font-size: 11px;
+    color: #787878;
+    font-family: 'Nanum Gothic', sans-serif;
+    margin: 2px 0 0 10px;
+}
+.menu2-body{
+    border: 1px solid #e3e3e3;
+    padding: 10px 10px 10px 0;
+    display: inline-block;
+}
+.clear{
+    float: left;
+    margin-left: 20px;
+    list-style: none;
+    display: block;
+}
+.clear:after {
+    display: block;
+    content: '';
+    clear: both;
+}
+.clear li{
+    float: left;
+    width: 100px;
+    text-align: center;
+    list-style: none;
+    position: relative;
+}
+.num{
+    margin: 5px 0 0;
+    text-align: center;
+    display: inline-block;
+    background: #f3f3f3;
+    border-radius: 20px;
+    width: 30px;
+    height: 30px;
+    line-height: 30px;
+}
+.num-text{
+    display: block;
+    text-align: center;
+    font-size: 11px;
+    font-weight: bold;
+    margin: 5px 0 0;
+}
+.icon-arr{
+    position: absolute;
+    display: block;
+    top: 11px;
+    right: 9px;
+    width: 10px;
+    height: 20px;
+    background: url(https://lush.co.kr/data/skin/front/howling/img/etc/icon-arr.png) no-repeat top right;
+}
+.order-total{
+    overflow: hidden;
+    padding: 2px 55px 2px 65px;
+    border-left: 1px solid #e3e3e3;
+    width: 100px;
+    float: right;
+    margin-left: 8.5px;
+}
+.order1{
+    display: block;
+    padding: 4px 0;
+}
+.title{
+    font-weight: bold;
+    font-family: 'Nanum Gothic', sans-serif;
+    font-size: 12px;
+    color: #333;
+}
+.order1 .num1{
+    float: right;
+    color: #7d7d7d;
+    font-size: 11px;
+    padding-top: 5px;
+}
+.menu3-body{
+    display: block;
+}
+.goods-board, .goods-board1{
+    width: 100%;
+    border-top: 1px solid #000;
+    border-collapse: collapse;
+    vertical-align: middle;
+}
+thead{
+    display: table-header-group;
+    vertical-align: middle;
+    border-color: inherit;
+}
+th{
+    padding: 5px 0;
+    border-bottom: 1px solid #e7e7e7;
+    font-size: 12px;
+    color: #8f8f8f;
+}
+tbody{
+    display: table-row-group;
+    vertical-align: middle;
+    border-color: inherit;
+}
+.goods-board td{
+    height: 75px;
+    padding: 10px 0;
+    text-align: center;
+    color: #797979;
+    border-bottom: 1px solid #e7e7e7;
+    font-size: 14px;
+}
+.goods-board1 td{
+    height: 75px;
+    padding: 10px 0;
+    text-align: center;
+    font-size: 14px;
+    color: #333333;
+}
+</style>
