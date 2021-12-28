fbaf424ef1654ae9c70636a1ebad9788635e4404
[Insa14] <[kong3361@naver.com]>
2021-12-28 11:30:17 +0900

    select 수정 중

--- src/components/MyReview.vue
+++ src/components/MyReview.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="content">
+    <div class="parent">
         <div class="content-inner">
             <div class="header">
                 <h2>나의플러스리뷰</h2>
@@ -15,7 +15,7 @@
                                 {{productList.name}}
                             </div>
                             <div class="rating">
-                                <star-rating :rating="reviewList.rating" :read-only="true" :increment="1" active-color="#333" inactive-color="#fff" :border-width="2"  :star-size="10" :show-rating="false"></star-rating>
+                                <star-rating :rating="reviewList.rating" :read-only="true" :increment="0.5" active-color="#333" :star-size="15" :show-rating="false"></star-rating>
                             </div>
                         </div>
                         <div class="date_info">
@@ -36,13 +36,46 @@
         </div>
     </div>
     <el-dialog v-model="showModalUpdate" title="상품후기 수정하기">
-        
+        <div class="select-option">
+            <div class="select-box" @click="openOption">
+                <span class="selected">{{selected}}</span>
+                <img :src="select_arrow_down">
+            </div>
+            <div name class="options-container" v-if="isOpen">
+                <input type="radio" id="5" v-model="selected" value="5" >
+                <label for="5">
+                    <star-rating :rating="5" :read-only="true" :increment="0.5" active-color="#ffcc00" :star-size="15" :show-rating="false"></star-rating>
+                    아주만족
+                </label>
+                <input type="radio" id="4" v-model="selected" value="4" >
+                <label for="4">
+                    <star-rating :rating="4" :read-only="true" :increment="0.5" active-color="#ffcc00" :star-size="15" :show-rating="false"></star-rating>
+                </label>
+                <label class="feel">만족</label>
+                <input type="radio" id="3" v-model="selected" value="3" >
+                <label for="3">
+                    <star-rating :rating="3" :read-only="true" :increment="0.5" active-color="#ffcc00" :star-size="15" :show-rating="false"></star-rating>
+                </label>
+                <label class="feel">보통</label>
+                <input type="radio" id="2" v-model="selected" value="2" >
+                <label for="2">
+                    <star-rating :rating="2" :read-only="true" :increment="0.5" active-color="#ffcc00" :star-size="15" :show-rating="false"></star-rating>
+                </label>
+                <label class="feel">미흡</label>
+                <input type="radio" id="1" v-model="selected" value="1" >
+                <label for="1">
+                    <star-rating :rating="1" :read-only="true" :increment="0.5" active-color="#ffcc00" :star-size="15" :show-rating="false"></star-rating>
+                </label>
+                <label class="feel">불만족</label>
+            </div>
+        </div>
     </el-dialog>
 </template>
 
 <script>
 import axios from 'axios';
 import StarRating from 'vue-star-rating';
+import select_arrow_down from '@/assets/select_arrow_down.png';
     export default {
         data(){
             return{
@@ -50,7 +83,10 @@ import StarRating from 'vue-star-rating';
                 itemcode : [],
                 productList : [],
                 token : sessionStorage.getItem("TOKEN"),
-                showModalUpdate : false
+                showModalUpdate : false,
+                selected : '', 
+                isOpen :false,
+                select_arrow_down : select_arrow_down,
             }
         },
         components:{
@@ -60,6 +96,9 @@ import StarRating from 'vue-star-rating';
             await this.handleReview();
         },
         methods : {
+            openOption(){
+                this.isOpen = !this.isOpen;
+            },
             async handleReview(){
                 const url = `/product/review`;
                 const headers = { 
@@ -103,9 +142,8 @@ import StarRating from 'vue-star-rating';
 }
 .content-inner{
     margin: 50px 0 0;
-    /* background: black; */
-    display: flex;
-    flex-direction: column;
+    background: black;
+    display: block;
 }
 .header{
     display: block;
@@ -129,6 +167,7 @@ import StarRating from 'vue-star-rating';
     border: 1px solid #e8e8e8;
     border-bottom: solid 1px #dbdbdb;
     display: inline-block;
+    width: 100%;
 }
 .review_img{
     width: 105px;
@@ -208,5 +247,69 @@ import StarRating from 'vue-star-rating';
     width: 64px;
     height: 24px;
     margin-right: 15px;
+}
+.select-option{
+    width: 95%;
+    display: block;
+    margin-left: 10px;
+    padding-top: 20px;
+    border-top: 1px solid #575757;
+}
+.select-box{
+    border: 1px solid rgb(197, 197, 197);
+    color : rgb(117, 117, 117);
+    overflow: hidden;
+    width: 100%;
+    height:37px;
+    position:relative;
+    display: inline-block;
+}
+.select-box > img {
+    width: 15px;
+    height: 8px;
+    top: 40%;
+    right: 2%;
+    position: absolute;
+}
+.selected {
+    width: 110px;
+    height: 100%;
+    /* border: 1px solid black; */
+    background: rgb(255, 255, 255);
+    color : rgb(37, 36, 36);
+    position: absolute;
+    text-align: left;
+    font-size: 15px;
+    padding: 10px 0px 8px 8px;
+}
+.options-container{
+    display: block;
+    color:black;
+    border: 1px solid rgb(197, 197, 197);
+    width: 100%;
+    height: fit-content;
+    opacity: 1;
+    background: white;
+    flex-flow: column;
+    transform: translateY(-2.2%);
+    z-index: 100000;
+}
+.options-container label {
+    display: block;
+    width: 98%;
+    color: rgb(37, 36, 36);
+    text-align: left;
+    font-size: 15px;
+    line-height: 0.6em;
+    padding: 10px 0px 8px 8px;
+}
+.options-container label:hover{
+    background: rgb(228, 228, 228);
+}
+.feel{
+    right: 2%;
+}
+input[type="radio"]{
+    display: none;
 }
 </style>
